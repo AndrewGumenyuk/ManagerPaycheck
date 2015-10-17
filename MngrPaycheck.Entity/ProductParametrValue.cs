d1dc6d84c280a9a0b3966a30b7f00fc02d3660fd
AndrewGumenyuk <dj_revo_1@mail.ru>
2015-10-17 20:16:51 +0100

    Intial commit
    - Added kind of additional project for interaction project with GUI.
    - Added in MngrPaycheck.Logics two patterns it are State and Strategy .
    - Add:ed EntityFramework, repository pattern
    - Added homepage for Administrator

---
+++ MngrPaycheck.Entity/ProductParametrValue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MngrPaycheck.Entity
+{
+    public class ProductParametrValue
+    {
+        public ProductParametrValue()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Required] [Key]
+        public Guid Id { get; set; }
+
+        public string Value { get; set; }
+
+        public Guid ProductID { get; set; }
+
+        #region properties
+        public virtual Product Product { get; set; }
+        #endregion
+    }
+}
---
+++ MngrPaycheck.Entity/Purchase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MngrPaycheck.Entity
+{
+    public class Purchase
+    {
+        public Purchase()
+        {
+            this.Id = Guid.NewGuid();
+            this.Products = new HashSet<Product>();
+        }
+        
+        [Required] [Key]
+        public Guid Id { get; private set; }
+
+        public float SumPurchase { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+        
+        public string PurchaseAdress { get; set; }
+
+        public bool Favorite { get; set; }
+
+        public Guid? SupermarketID { get; set; }
+        public Guid PaymentTypeID { get; set; }
+
+
+        #region properties
+        public virtual PaymentType PaymentType { get; set; }
+        public virtual Supermarket Supermarket { get; set; }
+        public virtual ICollection<Product> Products { get; set; } 
+        #endregion
+    }
+}
